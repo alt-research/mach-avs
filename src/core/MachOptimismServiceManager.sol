9d76888046074cba3f44637381ee97b9033cdfd5
xiaohuo <xiaohuo200@gmail.com>
2024-03-17 16:14:08 +0800

    wip: dep

--- src/core/MachOptimismServiceManager.sol
+++ src/core/MachOptimismServiceManager.sol
@@ -2,6 +2,21 @@
 pragma solidity ^0.8.9;
 
 import {ServiceManagerBase} from "eigenlayer-middleware/ServiceManagerBase.sol";
+import {IAVSDirectory} from "eigenlayer-core/contracts/interfaces/IAVSDirectory.sol";
+import {IRegistryCoordinator} from "eigenlayer-middleware/interfaces/IRegistryCoordinator.sol";
+import {IStakeRegistry} from "eigenlayer-middleware/interfaces/IStakeRegistry.sol";
 
 
-contract MachOptimismServiceManager {}
+contract MachOptimismServiceManager {
+
+     constructor(
+        IAVSDirectory __avsDirectory,
+        IRegistryCoordinator __registryCoordinator,
+        IStakeRegistry __stakeRegistry
+    )
+        BLSSignatureChecker(__registryCoordinator)
+        ServiceManagerBase(__avsDirectory, __registryCoordinator, __stakeRegistry)
+    {
+        _disableInitializers();
+    }
+}

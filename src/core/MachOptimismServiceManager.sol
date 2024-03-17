1387cb62d9a066555dffe12b5bcf8de3d3dc8568
xiaohuo <xiaohuo200@gmail.com>
2024-03-17 16:05:20 +0800

    refactor: clean repo

---
+++ src/core/MachOptimismServiceManager.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import {ServiceManagerBase} from "eigenlayer-middleware/ServiceManagerBase.sol";
+
+
+contract MachOptimismServiceManager {}

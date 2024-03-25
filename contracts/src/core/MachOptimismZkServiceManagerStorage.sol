39cda2decf280c2ae40908d6c168ba08e125c3e1
xiaohuo <xiaohuo200@gmail.com>
2024-03-25 22:00:25 +0800

    refactor: add more comment headers for zk service manager

--- contracts/src/core/MachOptimismZkServiceManagerStorage.sol
+++ contracts/src/core/MachOptimismZkServiceManagerStorage.sol
@@ -8,9 +8,28 @@
 
 pragma solidity ^0.8.12;
 
+import {IMachOptimismL2OutputOracle} from "../interfaces/IMachOptimismL2OutputOracle.sol";
+import {IRiscZeroVerifier} from "../interfaces/IMachOptimism.sol";
+import {IMachOptimism} from "../interfaces/IMachOptimism.sol";
+
 contract MachOptimismZkServiceManagerStorage {
     uint256 public immutable settlementChainID;
     uint256 public immutable rollupChainID;
+    IMachOptimismL2OutputOracle public l2OutputOracle;
+    IRiscZeroVerifier public verifier;
+    // The imageId for risc0 guest code.
+    bytes32 public imageId;
+
+    // Alerts for blocks, the tail is for earliest block.
+    // For the proved output, if there are exist a early block alert
+    // we will make it not proved!
+    IMachOptimism.L2OutputAlert[] internal l2OutputAlerts;
+    // The next index for no proved alert,
+    // `l2OutputAlerts[provedIndex - 1]` is the first no proved alerts,
+    // if is 0, means all alert is proved,
+    // if provedIndex == l2OutputAlerts.length, means all alert is not proved,
+    // the prover just need prove the earliest no proved alert,
+    uint256 public provedIndex;
 
     constructor(uint256 settlementChainID_, uint256 rollupChainID_) {
         settlementChainID = settlementChainID_;

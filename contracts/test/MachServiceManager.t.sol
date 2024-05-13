27a28df1e493903b89ecb65c4e02836c71f74ac7
noel <noel@altresear.ch>
2024-05-14 01:39:14 +0900

    test: MachServiceManager

--- contracts/test/MachServiceManager.t.sol
+++ contracts/test/MachServiceManager.t.sol
@@ -1,17 +1,31 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity =0.8.12;
 
-import "./AVSDeployer.sol";
-import "../src/error/Errors.sol";
+import "forge-std/Test.sol";
 
-contract MachServiceManagerTest is AVSDeployer {
+import "./BLSAVSDeployer.sol";
+import "../src/error/Errors.sol";
+import "../src/interfaces/IMachServiceManager.sol";
+
+contract MachServiceManagerTest is BLSAVSDeployer {
     event OperatorAllowed(address operator);
     event OperatorDisallowed(address operator);
     event AllowlistEnabled();
     event AllowlistDisabled();
+    event AlertConfirmerChanged(address previousAddress, address newAddress);
+    event WhitelisterChanged(address previousAddress, address newAddress);
+    event QuorumThresholdPercentageChanged(uint8 thresholdPercentages);
+    event RollupChainIdUpdated(uint256 previousRollupChainId, uint256 newRollupChainId);
+    event AlertConfirmed(bytes32 indexed alertHeaderHash, bytes32 messageHash);
 
     function setUp() public virtual {
         _deployMockEigenLayerAndAVS();
+
+        msgHash = keccak256(
+            abi.encode(IMachServiceManager.ReducedAlertHeader({messageHash: "foo", referenceBlockNumber: 201}))
+        );
+
+        _setAggregatePublicKeysAndSignature();
     }
 
     function test_AddToAllowlist() public {
@@ -110,6 +124,47 @@ contract MachServiceManagerTest is AVSDeployer {
         vm.startPrank(proxyAdminOwner);
         vm.expectRevert(AlreadyEnabled.selector); // Expect the specific revert for trying to enable an already enabled allowlist
         serviceManager.enableAllowlist();
+        vm.stopPrank();
+    }
+
+    function test_confirmAlert() public {
+        vm.startPrank(proxyAdminOwner);
+        serviceManager.disableAllowlist();
+        vm.stopPrank();
+
+        uint256 nonRandomNumber = 111;
+        uint256 numNonSigners = 1;
+        uint256 quorumBitmap = 1;
+        bytes memory quorumNumbers = BitmapUtils.bitmapToBytesArray(quorumBitmap);
+
+        (
+            uint32 referenceBlockNumber,
+            BLSSignatureChecker.NonSignerStakesAndSignature memory nonSignerStakesAndSignature
+        ) = _registerSignatoriesAndGetNonSignerStakeAndSignatureRandom(nonRandomNumber, numNonSigners, quorumBitmap);
+
+        (
+            BLSSignatureChecker.QuorumStakeTotals memory quorumStakeTotals,
+            /* bytes32 signatoryRecordHash */
+        ) = serviceManager.checkSignatures(msgHash, quorumNumbers, referenceBlockNumber, nonSignerStakesAndSignature);
+
+        bytes memory quorumThresholdPercentages = new bytes(1);
+        quorumThresholdPercentages[0] = bytes1(uint8(67));
+
+        IMachServiceManager.AlertHeader memory alertHeader = IMachServiceManager.AlertHeader({
+            messageHash: "foo",
+            quorumNumbers: quorumNumbers,
+            quorumThresholdPercentages: quorumThresholdPercentages,
+            referenceBlockNumber: referenceBlockNumber
+        });
+
+        vm.startPrank(proxyAdminOwner);
+        vm.expectEmit();
+        emit AlertConfirmed(msgHash, alertHeader.messageHash);
+        serviceManager.confirmAlert(alertHeader, nonSignerStakesAndSignature);
+
+        vm.expectRevert(AlreadyAdded.selector);
+        serviceManager.confirmAlert(alertHeader, nonSignerStakesAndSignature);
+
         vm.stopPrank();
     }
 }
